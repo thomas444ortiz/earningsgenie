e714b2635db86afa52c6583cef5f38adab15bc59
thomas444ortiz <thomasortiz444@gmail.com>
2023-07-12 22:28:04 -0700

    Work on login ui

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function LoginPage () {
-        return <h1>This is the login page</h1>
+        return (
+                <Box m="auto" sx={{  
+                        width: 300,
+                        height: 300,
+                        border:'2px dashed grey',
+                        borderRadius: '15px',
+                        marginTop: '100px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                }}>
+                        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                                <h2>Log In Below</h2>
+                                <TextField label="Email" sx={{ padding: "2px" }}/>
+                                <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ padding: "2px" }}/>
+                                <Button variant="contained" sx={{ marginTop: "10px" }}>Log In</Button>
+                                <RouterLink to="/register" style={{ marginTop: "20px", textAlign: "center" }}>Don't Have an Account? Create One</RouterLink>                                   
+                        </form>
+                </Box>      
+        );
 }
--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function RegisterPage () {
-        return <h1>This is the registration page</h1>
+    return (
+        <Box m="auto" sx={{  
+            width: 300,
+            height: 300,
+            border:'2px dashed grey',
+            borderRadius: '15px',
+            marginTop: '100px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <h2>Register Below</h2>
+                <TextField label="Email" sx={{ padding: "2px" }}/>
+                <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" sx={{ padding: "2px" }}/>
+                <Button variant="contained" sx={{ marginTop: "10px" }}>Create My Account</Button>
+                <RouterLink to="/login" style={{ marginTop: "20px", textAlign: "center" }}>Already have an account? Log in</RouterLink>                                 
+            </form>
+        </Box>     
+    );
 }

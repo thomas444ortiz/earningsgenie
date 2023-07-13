aa1976671bde92361c6c34560e590e3fb7d755bc
thomas444ortiz <thomasortiz444@gmail.com>
2023-07-13 09:40:10 -0700

    Work on filings ui

---
+++ frontend/src/components/companypage.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default function SeeCompany () {
+        return <h1>This is the page for a specific company</h1>
+}
--- frontend/src/components/seefilingspage.js
+++ frontend/src/components/seefilingspage.js
@@ -1,5 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
 
 export default function SeeFilingsPage() {
-        return <h1>This is the page to see filings</h1>
+  const options = ['Company 1', 'Company 2', 'Company 3', 'Company 4'];
+
+  return (
+    <Box m="auto" sx={{  
+      width: '90%',
+      border:'2px dashed grey',
+      borderRadius: '15px',
+      marginTop: '100px',
+      display: 'block',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '20px'
+    }}>
+      <h1 style={{textAlign: 'center'}}>Search for Companies Below</h1>
+      <Box sx={{width: '90%', margin: '0 auto'}}>
+        <Autocomplete
+          options={options}
+          fullWidth
+          renderInput={(params) => <TextField {...params} label="Enter a Ticker Here" />}
+        />
+        <Button variant="contained" sx={{marginTop: '20px'}}>Go To This Company's Documents</Button>
+      </Box>
+    </Box>
+  );
 }
+
